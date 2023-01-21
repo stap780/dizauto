49137f40a6d292faba4303ec75e13b5fa23644aa
stap780 <panaet80@mail.ru>
2023-01-21 14:29:27 +0300

    change gemfile

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,15 @@ module Dizauto
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Moscow'
+    config.active_record.default_timezone = :local
+   # config.eager_load_paths << Rails.root.join("extras")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-Rails.application.routes.draw do
+require 'sidekiq/web'
 
-  require 'sidekiq/web'
+Rails.application.routes.draw do
 
   authenticate :user, ->(user) { user.admin? } do
     mount Sidekiq::Web => '/sidekiq'
