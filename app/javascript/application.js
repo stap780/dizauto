dae7f5c1cf619e3c609570b35a4cf3c56541eb34
stap780 <panaet80@mail.ru>
2023-01-22 16:56:19 +0300

    add bootstrap alert close js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,31 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+window.bootstrap = bootstrap;
+
+document.addEventListener('turbo:load', () => {
+  console.log("DOM готов!");
   
-let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
-let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
-  return new bootstrap.Popover(popoverTriggerEl)  
-}) 
+  // var modal = new bootstrap.Modal('#typeForm');
+  // document.addEventListener('closeModal', () => {
+  //     modal.hide();
+  // });
+
+
+  const alertList = document.querySelectorAll('.alert');
+  if (alertList) {
+    const alerts = [...alertList].map(element => new bootstrap.Alert(element));
+    // console.log(alerts);
+    for (const alert of alerts) {
+      // console.log(alert);
+      setTimeout(() => {
+        alert.close();
+      }, 3000); // time in milliseconds  
+    };
+  }
+
+
+});
+
+
