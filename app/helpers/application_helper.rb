de46840b5da695787c9566ed2dfe165dea3eee38
stap780 <panaet80@mail.ru>
2023-04-13 12:28:33 +0300

    change wii pagination for bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,44 +1,44 @@
 module ApplicationHelper
-    class LinkRenderer < WillPaginate::ActionView::LinkRenderer
-      protected
+    # class LinkRenderer < WillPaginate::ActionView::LinkRenderer
+    #   protected
   
-      def html_container(html)
-        tag(:ul, html, class: "pagination")
-      end
+    #   def html_container(html)
+    #     tag(:ul, html, class: "pagination")
+    #   end
   
-      def previous_or_next_page(page, text, classname)
-        if page
-          tag(:li, link(text, page, class: "page-link"), class: "page-item")
-        else
-          tag(:li, tag(:a, text, class: "page-link"), class: "page-item disabled")
-        end
-      end
+    #   def previous_or_next_page(page, text, classname)
+    #     if page
+    #       tag(:li, link(text, page, class: "page-link"), class: "page-item")
+    #     else
+    #       tag(:li, tag(:a, text, class: "page-link"), class: "page-item disabled")
+    #     end
+    #   end
   
-      def page_number(page)
-        unless page == current_page
-          tag(:li, link(page, page, class: "page-link"), class: "page-item")
-        else
-          tag(:li, tag(:a, page, class: "page-link"), class: "page-item active")
-        end
-      end
+    #   def page_number(page)
+    #     unless page == current_page
+    #       tag(:li, link(page, page, class: "page-link"), class: "page-item")
+    #     else
+    #       tag(:li, tag(:a, page, class: "page-link"), class: "page-item active")
+    #     end
+    #   end
   
-      def gap
-        tag(:li, tag(:a, '&hellip;', class: "page-link"), class: "page-item disabled")
-      end
+    #   def gap
+    #     tag(:li, tag(:a, '&hellip;', class: "page-link"), class: "page-item disabled")
+    #   end
   
-    end ## end of class LinkRenderer
+    # end ## end of class LinkRenderer
   
-    def bs_will_paginate(collection = nil, options = {})
-      options, collection = collection, nil if collection.is_a? Hash
-      options = options.merge(
-        renderer: ApplicationHelper::LinkRenderer,
-        previous_label: "&laquo;",
-        next_label: "&raquo;"
-      )
-      will_paginate(collection, options)
-    end
+  def bs_will_paginate(collection = nil, options = {})
+    options, collection = collection, nil if collection.is_a? Hash
+    options = options.merge(
+      renderer: BootstrapPaginateRenderer, #ApplicationHelper::LinkRenderer,
+      previous_label: "&laquo;",
+      next_label: "&raquo;"
+    )
+    will_paginate(collection, options)
+  end
 
-    def prepend_flash
+  def prepend_flash
       turbo_stream.prepend 'our_flash', partial: 'shared/flash'
   end
   
