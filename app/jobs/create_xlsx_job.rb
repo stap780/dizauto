98862df4ac05dabd7e2039127349a0ee71f61b8f
stap780 <panaet80@mail.ru>
2024-09-04 14:24:11 +0300

    add retry to xlsx job

--- app/jobs/create_xlsx_job.rb
+++ app/jobs/create_xlsx_job.rb
@@ -1,23 +1,22 @@
 class CreateXlsxJob < ApplicationJob
-  include Sidekiq::Job
   queue_as :print
+  #retry_on AlwaysRetryException, attempts: :unlimited
+  retry_on AlwaysRetryException, attempts: 1
 
   def perform(collection_ids, options = {})
-    return if cancelled?
-
     model = options[:model]
     items = model.camelize.constantize.where(id: collection_ids)
 
-    success, message = Product::CreateXlsx.call(items, {model: "Product"})
+    success, message = Product::CreateXlsx.call(items, {model: "Product"} )
     if success
       PrintNotifier.with(
-        record: items.first,
-        message: "Success #{message}",
-        blob: nil,
-        error: nil,
-        model: model,
-        template: "xlsx"
-      ).deliver(User.find_by_id(options[:current_user_id]))
+                          record: items.first, 
+                          message: "Success #{message}",
+                          blob: nil,
+                          error: nil,
+                          model: model,
+                          template: 'xlsx'
+                        ).deliver(User.find_by_id(options[:current_user_id]))
 
       Turbo::StreamsChannel.broadcast_replace_to(
         User.find(options[:current_user_id]),
@@ -29,13 +28,13 @@ class CreateXlsxJob < ApplicationJob
       )
     else
       PrintNotifier.with(
-        record: items.first,
-        message: "Error",
-        blob: nil,
-        error: message,
-        model: model,
-        template: "xlsx"
-      ).deliver(User.find_by_id(options[:current_user_id]))
+                          record: items.first, 
+                          message: "Error",
+                          blob: nil,
+                          error: message,
+                          model: model,
+                          template: 'xlsx'
+                        ).deliver(User.find_by_id(options[:current_user_id]))
 
       Turbo::StreamsChannel.broadcast_replace_to(
         User.find(options[:current_user_id]),
@@ -47,12 +46,5 @@ class CreateXlsxJob < ApplicationJob
       )
     end
   end
-
-  def cancelled?
-    Sidekiq.redis { |c| c.exists("cancelled-#{jid}") == 1 } # Use c.exists? on Redis >= 4.2.0
-  end
-
-  def self.cancel!(jid)
-    Sidekiq.redis { |c| c.set("cancelled-#{jid}", 1, ex: 86_400) }
-  end
+  
 end
