dbc21e934e08c5952c6791e3da33badc961e6fab
stap780 <panaet80@mail.ru>
2024-12-26 14:51:05 +0300

    add telegram bot

--- config/initializers/telegram_bot.rb
+++ config/initializers/telegram_bot.rb
@@ -2,5 +2,5 @@ require 'telegram/bot'
 
 Rails.application.config.after_initialize do
   puts 'Running Telegram Bot Job!'
-  # TelegramBotJob.perform_later(TelegramBot.first.token) if TelegramBot.first.present?
+  TelegramBotJob.perform_later(TelegramBot.first.token) if TelegramBot.first.present?
 end
